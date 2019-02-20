e9418ebfc660a7088c7fbcbfc86dc6ac4a7563dc
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2019-02-20 15:44:27 +0600

    Channels: ws connect

--- miscellaneous/django-channels/django-channels/chat/consumers.py
+++ miscellaneous/django-channels/django-channels/chat/consumers.py
@@ -3,16 +3,24 @@ import asyncio
 from channels.db import DatabaseSyncToAsync
 from channels.consumer import AsyncConsumer
 from django.contrib.auth import get_user_model
+from loguru import logger
 
 from .models import Thread, ChatMessage
 
 
 class ChatConsumer(AsyncConsumer):
-    def websocket_connect(self, event):
-        print('connect', event)
+    async def websocket_connect(self, event):
+        await self.send({
+            'type': 'websocket.accept'
+        })
 
-    def websocket_receive(self, event):
+        # current_username = self.scope
+        other_username = self.scope['url_route']['kwargs']['username']
+        logger.debug(self.scope)
+
+
+    async def websocket_receive(self, event):
         print('receive', event)
 
-    def websocket_disconnect(self, event):
+    async def websocket_disconnect(self, event):
         print('disconnect', event)
--- miscellaneous/django-channels/django-channels/djangounchained/routing.py
+++ miscellaneous/django-channels/django-channels/djangounchained/routing.py
@@ -3,16 +3,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-from chat.consumers import ChatConsumer
+import chat.routing
 
 application = ProtocolTypeRouter({
     # Empty for now (http->django views is added by default)
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
-                [
-                    url(r"messages/(?P<username>[\w.@+-])/$", ChatConsumer)
-                ]
+                chat.routing.websocket_urlpatterns
             )
         )
     )
