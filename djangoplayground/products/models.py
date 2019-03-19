31eac9e1636e56e440124f720b408a6f6381558c
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2019-03-19 11:59:47 +0600

    Django: product model

--- djangoplayground/products/models.py
+++ djangoplayground/products/models.py
@@ -2,7 +2,11 @@ from django.db import models
 
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.DecimalField(max_digits=10, decimal_places=5)
-    summary = models.TextField(default='this is cool!')
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=1000, decimal_places=2)
+    summary = models.TextField(blank=True, null=True)
+    featured = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
