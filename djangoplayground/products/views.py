4ca53d37eac89d11fc0a3ff8cadf0f344bd1034f
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2019-03-21 01:13:24 +0600

    Django: product list and detail

--- djangoplayground/products/views.py
+++ djangoplayground/products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Product
+
+
+def products_list_view(request):
+    products = Product.objects.all()
+
+    return render(request, 'products/list.html', {
+        'products': products
+    })
+
+
+def products_details_view(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+
+    return render(request, 'products/detail.html', {
+        'product': product
+    })
