c67ad989b97809d5d5a0315c1d6366e8029fdedb
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2019-04-07 17:37:44 +0600

    Blog: templating

--- blog/webpack.mix.js
+++ blog/webpack.mix.js
@@ -1,9 +1,17 @@
 const mix = require('laravel-mix');
 
+mix.copyDirectory('node_modules/open-iconic/font/fonts', 'resources/static/fonts');
+
 mix.styles([
-    'node_modules/bootstrap/dist/css/bootstrap.css'
+    'node_modules/bootstrap/dist/css/bootstrap.css',
+    'node_modules/open-iconic/font/css/open-iconic-bootstrap.css',
 ], 'resources/static/css/vendors.css');
 
 mix.styles([
     'resources/assets/css/app.css'
 ], 'resources/static/css/app.css');
+
+mix.scripts([
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/bootstrap/dist/js/bootstrap.js'
+], 'resources/static/js/vendors.js');
