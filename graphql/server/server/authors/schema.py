2f915cf4006ba6b795e81d113ef5d663509d19da
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2019-05-05 22:33:58 +0600

    GraphQL: author and books relation

--- graphql/server/server/authors/schema.py
+++ graphql/server/server/authors/schema.py
@@ -1,6 +1,16 @@
 import graphene
 
 
+def get_book_type():
+    from server.books.schema import BookType
+    return BookType
+
+
+def get_books():
+    from server.books.schema import books
+    return books
+
+
 class AuthorType(graphene.ObjectType):
     name = 'Author'
     description = '...'
@@ -8,6 +18,16 @@ class AuthorType(graphene.ObjectType):
     id = graphene.ID()
     author_name = graphene.String()
     age = graphene.Int()
+    books = graphene.List(get_book_type)
+
+    def resolve_books(self, info, **kwargs):
+        response = []
+        books = get_books()
+        for book in books:
+            if self.id == book.author:
+                response.append(book)
+
+        return response
 
 
 # dummy data
