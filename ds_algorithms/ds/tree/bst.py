39e93eb5738dac8e9020ee59fb8c040539ecd371
Sazzadur Rahman <sazzad.rahman.bd@gmail.com>
2020-02-10 02:50:56 +0600

    ds_algo: binary search tree

---
+++ ds_algorithms/ds/tree/bst.py
@@ -0,0 +1,68 @@
+import json
+
+
+class Node(object):
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+    def __str__(self):
+        response = {
+            "value": self.value,
+            "left": self.left.value if self.left else None,
+            "right": self.right.value if self.right else None
+        }
+        return json.dumps(response)
+
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self._root = None
+
+    def _traverse(self, node):
+        tree = {"value": node.value}
+        tree["left"] = self._traverse(node.left) if node.left else None
+        tree["right"] = self._traverse(node.right) if node.right else None
+        return tree
+
+    def get_root(self):
+        return self._root
+
+    def traverse(self):
+        root = self._root
+        response = self._traverse(root)
+        return response
+
+    def insert(self, value):
+        node = Node(value=value)
+        if self._root is None:
+            self._root = node
+            return self
+        else:
+            current_node = self._root
+            while True:
+                if current_node.value > node.value:
+                    if current_node.left is None:
+                        current_node.left = node
+                        return self
+                    current_node = current_node.left
+                else:
+                    if current_node.right is None:
+                        current_node.right = node
+                        return self
+                    current_node = current_node.right
+
+    def search(self, value):
+        def _search(current_node_nested, value_nested):
+            if current_node_nested.value == value_nested:
+                return current_node_nested
+            elif current_node_nested.value > value_nested and current_node_nested.left is not None:
+                return _search(current_node_nested.left, value_nested)
+            elif current_node_nested.value < value_nested and current_node_nested.right is not None:
+                return _search(current_node_nested.right, value_nested)
+            else:
+                return None
+
+        current = self._root
+        return _search(current, value)
